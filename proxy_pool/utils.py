677843924c7f74b915a40cbdc7270851abf1f5a9
AllegroMind <92263399+CarmenFYJ@users.noreply.github.com>
2025-06-30 11:47:56 +0800

    utils.py

---
+++ proxy_pool/utils.py
@@ -0,0 +1,15 @@
+import aiohttp
+import asyncio
+
+async def verify_proxy_async(proxy_url, test_url="http://httpbin.org/ip", timeout=5):
+    """
+    异步验证代理是否可用，访问一个简单的测试URL
+    proxy_url: 形如 'http://ip:port'
+    """
+    try:
+        conn = aiohttp.TCPConnector(ssl=False)  # 有些代理https可能出错
+        async with aiohttp.ClientSession(connector=conn) as session:
+            async with session.get(test_url, proxy=proxy_url, timeout=timeout) as resp:
+                return resp.status == 200
+    except:
+        return False
